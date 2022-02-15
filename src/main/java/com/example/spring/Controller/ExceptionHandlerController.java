9ccbf28d98461c7b7e5b9fe0d705afce2eb93459
vmaki <hmarax3@gmail.com>
2022-02-15 12:35:17 +0200

    remover some dependency from pom.xml

--- src/main/java/com/example/spring/Controller/ExceptionHandlerController.java
+++ src/main/java/com/example/spring/Controller/ExceptionHandlerController.java
@@ -1,6 +1,5 @@
 package com.example.spring.Controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,11 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @ControllerAdvice
-@Slf4j
 public class ExceptionHandlerController {
     @ExceptionHandler(Exception.class)
     public ModelAndView handleError(HttpServletRequest req, HttpServletResponse resp, Exception ex) {
-        log.error("Request: " + req.getRequestURL() + " raised " + ex);
         ModelAndView mav = new ModelAndView();
         mav.addObject("exception", ex.getMessage());
         mav.addObject("url", req.getRequestURL());
